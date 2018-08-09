aa63a44d336e3b79545cb384c7a92c8edf0fdcfc
Angel Rodriguez <arrmixer@gmail.com>
2018-08-08 21:50:44 -0400

    finish custom gradle task the gradle task was optional but fun had to make a task that started the google server, run the unit test (mine came from my jokes library), run the espresso tests, and then finally stop the server

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -4,18 +4,27 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
+import android.widget.TextView;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
@@ -25,21 +34,29 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void init(){
+    public void init() {
         mActivityTestRule.getActivity().getSupportFragmentManager().beginTransaction();
+
+        List<String> jokes = new ArrayList<>();
+        jokes.add("What do you call a fish without eyes? Fsh.");
+        jokes.add("Why did the scarecrow win an award? Because he was outstanding in his field.");
+        jokes.add("What lights up a soccer stadium? A soccer match.");
+        jokes.add("Why shouldn’t you write with a broken pencil? Because it’s pointless. ");
+        jokes.add("What’s the difference between the bird flu and the swine flu? One requires tweetment and the other an oinkment.");
+        jokes.add("When do computers overheat? When they need to vent.");
     }
 
     //make sure the textView is visible and with the correct text shown
     @Test
-    public void TestTextVisible(){
-        ViewInteraction textView  = onView(withId(R.id.instructions_text_view));
+    public void TestTextVisible() {
+        ViewInteraction textView = onView(withId(R.id.instructions_text_view));
         textView.check(matches(isDisplayed()));
         textView.check(matches(allOf(withText("Press the button for a delicious joke!"))));
     }
 
     //make sure the button is visible and with the correct text shown
     @Test
-    public void ButtonIsVisible(){
+    public void ButtonIsVisible() {
         ViewInteraction button = onView(withId(R.id.instructions_button));
         button.check(matches(isDisplayed()));
         button.check(matches(allOf(withText("Tell Joke"))));
@@ -48,14 +65,18 @@ public class MainActivityTest {
 
     //shows textView in Android Library androidJokes
     @Test
-    public void textViewIsVisible(){
+    public void textViewIsVisible() {
         ViewInteraction button = onView(withId(R.id.instructions_button));
         button.perform(click());
 
         ViewInteraction textView = onView(withId(R.id.txt_android_jokes));
         textView.check(matches(isDisplayed()));
-        textView.check(matches(allOf(withText("What do you call a fish without eyes? Fsh."))));
+        //doesn't work with randomly selected jokes
+//        textView.check(matches(allOf(withText("What do you call a fish without eyes? Fsh."))));
+        textView.check(matches(not(withText(" "))));
     }
+
+
 
 
 }
